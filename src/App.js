3aef7534c33bd25e074ddccc65545dc9ba657681
Yvonne H <yvonnehelland@outlook.com>
2021-04-22 18:31:55 +0200

    Ant layout design test

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import Hotels from "./pages/Hotels";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
-import Shotel from "./pages/Shotel";
+import MyLayout from "./components/layout/MyLayout";
+import "antd/dist/antd.css";
 import "./sass/main.scss";
 
 function App() {
   return (
     <>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/hotels" exact component={Hotels} />
-          <Route path="/contact" exact component={Contact} />
-          <Route path="/login" exact component={Login} />
-        </Switch>
-      </Router>
+      <MyLayout>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/hotels" exact component={Hotels} />
+            <Route path="/contact" exact component={Contact} />
+            <Route path="/login" exact component={Login} />
+          </Switch>
+        </Router>
+      </MyLayout>
     </>
   );
 }
---
+++ src/components/layout/MyLayout.js
@@ -0,0 +1,28 @@
+import { Layout, Menu, Breadcrumb } from "antd";
+const { Header, Content, Footer } = Layout;
+
+const MyLayout = () => {
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: "0 50px" }}>
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content">Content</div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>© Yvonne Helland 2021</Footer>
+    </Layout>
+  );
+};
+
+export default MyLayout;
