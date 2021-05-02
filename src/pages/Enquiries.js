ff79374f7839d58bba9379e2f14afa3d3f164235
Yvonne H <yvonnehelland@outlook.com>
2021-05-03 01:33:23 +0200

    Creates new pages

---
+++ src/pages/Enquiries.js
@@ -0,0 +1,15 @@
+import Heading from "../components/Heading";
+import MyLayout from "../components/layout/MyLayout";
+
+const Enquiries = () => {
+  return (
+    <>
+      <MyLayout>
+        <Heading heading="Bookings" />
+        <Heading heading="Enquries" />
+      </MyLayout>
+    </>
+  );
+};
+
+export default Enquiries;
