fbfce9907b57502be9fdb91473bd18cecbe764bd
Yvonne H <yvonnehelland@outlook.com>
2021-05-03 01:14:16 +0200

    Update for Shotel page + App.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import Places from "./pages/Places";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
+import Shotel from "./pages/Shotel";
+import { AuthProvider } from "./context/AuthContext";
 import "antd/dist/antd.css";
 import "./sass/main.scss";
 
 function App() {
   return (
     <>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/places-to-stay" exact component={Places} />
-          <Route path="/contact" exact component={Contact} />
-          <Route path="/login" exact component={Login} />
-        </Switch>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/places-to-stay" exact component={Places} />
+            <Route path="/contact" exact component={Contact} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/places-to-stay/:id" exact component={Shotel} />
+          </Switch>
+        </Router>
+      </AuthProvider>
     </>
   );
 }
--- src/pages/Shotel.js
+++ src/pages/Shotel.js
@@ -1,11 +1,56 @@
 import MyLayout from "../components/layout/MyLayout";
 import Heading from "../components/Heading";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
 
 const Shotel = () => {
+  const { id } = useParams();
+  const [hotel, setHotel] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchHotel = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/${id}`);
+        console.log(response);
+        setHotel(response.data);
+      } catch (error) {
+        setError(error.toString());
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchHotel();
+  }, [id]);
+
+  if (loading) {
+    return (
+      <div>
+        <p>The content is loading... Please hold on.</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p>There has been an error. We apologize for the inconvenience.</p>
+      </div>
+    );
+  }
+
   return (
     <>
       <MyLayout>
         <Heading heading="Specific hotel" />
+        <p>{hotel.symbol}</p>
+        <p>{hotel.name}</p>
+        <p>{hotel.description.en}</p>
+        <p>Current price: {hotel.market_data.current_price.nok} NOK</p>
+        <img src={hotel.image.small} alt="coin" />
       </MyLayout>
     </>
   );
