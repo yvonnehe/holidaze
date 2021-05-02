e4ba98624a37e60895e4e5ddac75dd8d1d1afd8f
Yvonne H <yvonnehelland@outlook.com>
2021-05-02 23:48:59 +0200

    Button style update

--- src/components/layout/HomeLayout.js
+++ src/components/layout/HomeLayout.js
@@ -37,7 +37,7 @@ const HomeLayout = ({ children }) => {
           </Link>
         </div>
         <button className="logbutton logbutton--blue">
-          <Link className="loglink" to="/login">
+          <Link className="loglink--light" to="/login">
             Log in
           </Link>
         </button>
