1593381daed46878fb1b3b408ef1f5476d21cec8
Yvonne H <yvonnehelland@outlook.com>
2021-05-05 20:01:08 +0200

    Adds search

--- src/pages/Places.js
+++ src/pages/Places.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
+import { Row, Col } from "antd";
 
 const Places = () => {
   const [hotels, setHotels] = useState([]);
@@ -25,6 +26,77 @@ const Places = () => {
     fetchHotels();
   }, []);
 
+  //filtering
+  const [filteredHotels, setFilteredHotels] = useState([]);
+  const [filtered, setFiltered] = useState(false);
+
+  const handleFiltering = (e) => {
+    let filterHotels = hotels.filter((hotel) => {
+      return hotel.name.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    setFilteredHotels(filterHotels);
+    setFiltered(true);
+  };
+  const filterinput = document.querySelector(".filter");
+  if (filterinput === "") {
+    setFiltered(false);
+  }
+
+  if (filtered === true) {
+    return (
+      <>
+        <MyLayout>
+          <Heading heading="Places to stay" />
+          <input
+            className="filter"
+            type="text"
+            name="query"
+            onChange={handleFiltering}
+          />
+          {filteredHotels.map((hotel) => {
+            return (
+              <div key={hotel.id} className="hoteldiv">
+                <Row>
+                  <Col sm={24} lg={9}>
+                    <div>
+                      <img
+                        src={hotel.img}
+                        alt={hotel.name}
+                        style={{ width: "100%" }}
+                        className="hotelimg"
+                      />
+                    </div>
+                  </Col>
+                  <Col sm={24} lg={11}>
+                    <Link
+                      to={`/places-to-stay/${hotel.id}`}
+                      className="hotelinfo"
+                    >
+                      <p>{hotel.shortdescription}</p>
+                      <h3>{hotel.name}</h3>
+                      <hr></hr>
+                      <p>{hotel.extras}</p>
+                      <p>{hotel.distance}km from Bergen city centre</p>
+                    </Link>
+                  </Col>
+                  <Col sm={24} lg={4}>
+                    <Link
+                      to={`/places-to-stay/${hotel.id}`}
+                      className="hotelinfo"
+                    >
+                      <p>{hotel.price} NOK</p>
+                      <p>Per night including taxes</p>
+                    </Link>
+                  </Col>
+                </Row>
+              </div>
+            );
+          })}
+        </MyLayout>
+      </>
+    );
+  }
+
   if (loading) {
     return (
       <div>
@@ -45,14 +117,48 @@ const Places = () => {
     <>
       <MyLayout>
         <Heading heading="Places to stay" />
+        <input
+          className="filter"
+          type="text"
+          name="query"
+          onChange={handleFiltering}
+        />
         {hotels.map((hotel) => {
           return (
             <div key={hotel.id} className="hoteldiv">
-              <p>{hotel.shortdescription}</p>
-              <p>{hotel.name}</p>
-              <Link to={`/places-to-stay/${hotel.id}`}>
-                <button>Read more</button>
-              </Link>
+              <Row>
+                <Col sm={24} lg={9}>
+                  <div>
+                    <img
+                      src={hotel.img}
+                      alt={hotel.name}
+                      style={{ width: "100%" }}
+                      className="hotelimg"
+                    />
+                  </div>
+                </Col>
+                <Col sm={24} lg={11}>
+                  <Link
+                    to={`/places-to-stay/${hotel.id}`}
+                    className="hotelinfo"
+                  >
+                    <p>{hotel.shortdescription}</p>
+                    <h3>{hotel.name}</h3>
+                    <hr></hr>
+                    <p>{hotel.extras}</p>
+                    <p>{hotel.distance}km from Bergen city centre</p>
+                  </Link>
+                </Col>
+                <Col sm={24} lg={4}>
+                  <Link
+                    to={`/places-to-stay/${hotel.id}`}
+                    className="hotelinfo"
+                  >
+                    <p>{hotel.price} NOK</p>
+                    <p>Per night including taxes</p>
+                  </Link>
+                </Col>
+              </Row>
             </div>
           );
         })}
