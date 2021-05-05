d1ddd03538d95b63574aadbeb4e59e3603f2c51e
Yvonne H <yvonnehelland@outlook.com>
2021-05-05 20:00:29 +0200

    Pushes you back to login if you try to access admin page without being logged in

--- src/pages/Enquiries.js
+++ src/pages/Enquiries.js
@@ -1,7 +1,17 @@
 import Heading from "../components/Heading";
 import MyLayout from "../components/layout/MyLayout";
+import { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 
 const Enquiries = () => {
+  const [auth] = useContext(AuthContext);
+  const history = useHistory();
+
+  if (!auth) {
+    history.push("/login");
+  }
+
   return (
     <>
       <MyLayout>
