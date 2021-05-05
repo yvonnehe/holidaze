530e20ed932a148dd4b5befd6dafbc969ec5abc4
Yvonne H <yvonnehelland@outlook.com>
2021-05-05 20:01:37 +0200

    Adds booking section

--- src/pages/Shotel.js
+++ src/pages/Shotel.js
@@ -4,12 +4,15 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
+import { DatePicker, Space } from "antd";
+import { Input, InputNumber } from "antd";
 
 const Shotel = () => {
   const { id } = useParams();
   const [hotel, setHotel] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const { RangePicker } = DatePicker;
 
   useEffect(() => {
     const fetchHotel = async () => {
@@ -42,15 +45,39 @@ const Shotel = () => {
     );
   }
 
+  function onChange(value) {
+    console.log("changed", value);
+  }
+
   return (
     <>
       <MyLayout>
-        <Heading heading="Specific hotel" />
-        <p>{hotel.name}</p>
+        <Heading heading={hotel.name} />
+        <img src={hotel.img} alt={hotel.name} />
         <p>{hotel.shortdescription}</p>
         <p>{hotel.description}</p>
-        <p>Current price: {hotel.price} NOK</p>
-        <img src={hotel.img} alt="hotel" />
+        <br></br>
+
+        <p>{hotel.price} NOK per night</p>
+        <div className="site-input-group-wrapper" style={{ width: "30%" }}>
+          <Space direction="vertical" size={12}>
+            <RangePicker />
+          </Space>
+          <Input.Group compact>
+            <Input
+              style={{ width: "60%", height: "42px" }}
+              defaultValue="Number of guests"
+            />
+            <InputNumber
+              style={{ width: "24%" }}
+              min={1}
+              max={99}
+              defaultValue={2}
+              onChange={onChange}
+            />
+          </Input.Group>
+          <button>Book now</button>
+        </div>
       </MyLayout>
     </>
   );
