0918277986e6b5e85eaf789d5dc4e8d33568ac5a
Yvonne H <yvonnehelland@outlook.com>
2021-05-12 15:21:38 +0200

    Adds new constant url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const BASE_URL = "https://secret-citadel-14886.herokuapp.com/hotels";
 export const BASE_URL_LOGIN = process.env.REACT_APP_STRAPI_URL;
 export const AUTH_PATH = process.env.REACT_APP_AUTH_PATH;
+export const CONTACT_URL =
+  "https://secret-citadel-14886.herokuapp.com/contacts";
