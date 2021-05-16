d7d92a58ee3c6e6cdef4fe8960f343723ae9edca
Yvonne H <yvonnehelland@outlook.com>
2021-05-16 19:16:07 +0200

    Style updates + adds success message

--- src/pages/Shotel.js
+++ src/pages/Shotel.js
@@ -9,6 +9,7 @@ import { Input, InputNumber } from "antd";
 import { useForm, Controller } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { Row, Col } from "antd";
 
 const schema = yup.object().shape({
   guests: yup.number().required(),
@@ -33,9 +34,9 @@ const Shotel = () => {
   const { register, handleSubmit, errors, control, watch } = useForm({
     resolver: yupResolver(schema),
   });
-  const [, setSubmitting] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
   const [, setPostError] = useState(null);
-  const [, setSuccess] = useState(null);
+  const [success, setSuccess] = useState(null);
   const formValues = watch();
   // constants for adding bookings
   const [, setBookings] = useState(null);
@@ -109,63 +110,81 @@ const Shotel = () => {
       <>
         <MyLayout>
           <Heading heading={hotel.name} />
-          <img src={hotel.img} alt={hotel.name} />
-          <p>{hotel.shortdescription}</p>
-          <p>{hotel.description}</p>
-          <br></br>
+          <Row gutter={[32, 32]}>
+            <Col sm={24} lg={12}>
+              <img
+                src={hotel.img}
+                alt={hotel.name}
+                className="shotelimg"
+                style={{ width: "100%" }}
+              />
+            </Col>
+            <Col sm={24} lg={12}>
+              <h4>About this space</h4>
+              <p>{hotel.shortdescription}</p>
+              <p>{hotel.description}</p>
+              <h4>Amenities</h4>
+              <p>{hotel.extras}</p>
+              <h4>Location</h4>
+              <p>{hotel.distance}km from Bergen city centre</p>
+              <br></br>
 
-          <p>{hotel.price} NOK per night</p>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            onChange={() => {
-              console.log("fack");
-            }}
-          >
-            <div className="site-input-group-wrapper" style={{ width: "30%" }}>
-              <Space direction="vertical" size={12}>
-                <Controller
-                  as={RangePicker}
-                  options={{ name: "data" }}
-                  control={control}
-                  name="date"
-                  onChange={([selected]) => {
-                    return { value: selected };
-                  }}
-                  defaultValue={{}}
-                />
-              </Space>
-              {errors.date && <span>{errors.date.message}</span>}
-              <Input.Group compact>
-                <Input
-                  style={{ width: "60%", height: "42px" }}
-                  defaultValue="Number of guests"
-                />
-                <InputNumber
-                  style={{ width: "24%" }}
-                  min={1}
-                  max={99}
-                  defaultValue={2}
-                  name="guests"
-                  ref={register}
-                />
-              </Input.Group>
-              {errors.guests && <span>{errors.guests.message}</span>}
-              <button
-                className="formbutton"
-                type="submit"
-                style={{ width: "84%" }}
-              >
-                Book now
-              </button>
-              <div>
-                {formValues.date &&
-                  Math.ceil(
-                    (formValues.date[1] - formValues.date[0]) /
-                      (1000 * 3600 * 24)
-                  ) * hotel.price}
-              </div>
-            </div>
-          </form>
+              <p>{hotel.price} NOK per night</p>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="bookingdiv">
+                  <div
+                    className="site-input-group-wrapper"
+                    style={{ width: "220px" }}
+                  >
+                    <Space direction="vertical" size={12}>
+                      <Controller
+                        as={RangePicker}
+                        options={{ name: "data" }}
+                        control={control}
+                        name="date"
+                        onChange={([selected]) => {
+                          return { value: selected };
+                        }}
+                        defaultValue={{}}
+                      />
+                    </Space>
+                    {errors.date && <span>{errors.date.message}</span>}
+                    <Input.Group compact>
+                      <Input
+                        style={{ width: "160.5px", height: "42px" }}
+                        defaultValue="Number of guests"
+                      />
+                      <InputNumber
+                        style={{ width: "59px" }}
+                        min={1}
+                        max={99}
+                        defaultValue={2}
+                        name="guests"
+                        ref={register}
+                      />
+                    </Input.Group>
+                    {errors.guests && <span>{errors.guests.message}</span>}
+                    <button
+                      className="formbutton"
+                      type="submit"
+                      style={{ width: "219px" }}
+                    >
+                      {submitting ? "Booking ..." : "Book now"}
+                    </button>
+                    <div>
+                      Total price{" "}
+                      {formValues.date &&
+                        Math.ceil(
+                          (formValues.date[1] - formValues.date[0]) /
+                            (1000 * 3600 * 24)
+                        ) * hotel.price}
+                    </div>
+                  </div>
+                </div>
+              </form>
+              {success ? <p>Booking complete!</p> : null}
+            </Col>
+          </Row>
         </MyLayout>
       </>
     );
