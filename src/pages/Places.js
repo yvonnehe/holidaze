5fe4faa180e81a47773ed4821625a1721f0ecb03
Yvonne H <yvonnehelland@outlook.com>
2021-05-19 18:02:42 +0200

    Places code simplification and search fix

--- src/pages/Places.js
+++ src/pages/Places.js
@@ -10,6 +10,7 @@ const Places = () => {
   const [hotels, setHotels] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [showSearchOptions, setShowSearchOptions] = useState(false);
 
   useEffect(() => {
     const fetchHotels = async () => {
@@ -17,6 +18,7 @@ const Places = () => {
         const response = await axios.get(`${BASE_URL}`);
         console.log(response.data);
         setHotels(response.data);
+        setFilteredHotels(response.data);
       } catch (error) {
         setError(error.toString());
       } finally {
@@ -27,13 +29,14 @@ const Places = () => {
   }, []);
 
   //filtering
-  const [filteredHotels, setFilteredHotels] = useState([]);
+  const [filteredHotels, setFilteredHotels] = useState(hotels);
   const [filtered, setFiltered] = useState(false);
 
   const handleFiltering = (e) => {
     let filterHotels = hotels.filter((hotel) => {
       return hotel.name.toLowerCase().includes(e.target.value.toLowerCase());
     });
+    setShowSearchOptions(e.target.value ? true : false);
     setFilteredHotels(filterHotels);
     setFiltered(true);
   };
@@ -42,7 +45,7 @@ const Places = () => {
     setFiltered(false);
   }
 
-  if (filtered === true) {
+  if (filteredHotels) {
     return (
       <>
         <MyLayout>
@@ -56,18 +59,19 @@ const Places = () => {
                 onChange={handleFiltering}
                 placeholder="Search..."
               />
-              {filteredHotels.map((hotel) => {
-                return (
-                  <div key={hotel.id} className="searchdiv">
-                    <Link
-                      to={`/places-to-stay/${hotel.id}`}
-                      className="hotelinfo"
-                    >
-                      {hotel.name}
-                    </Link>
-                  </div>
-                );
-              })}
+              {showSearchOptions &&
+                filteredHotels.map((hotel) => {
+                  return (
+                    <div key={hotel.id} className="searchdiv">
+                      <Link
+                        to={`/places-to-stay/${hotel.id}`}
+                        className="hotelinfo"
+                      >
+                        {hotel.name}
+                      </Link>
+                    </div>
+                  );
+                })}
             </div>
           </div>
           {filteredHotels.map((hotel) => {
@@ -131,59 +135,15 @@ const Places = () => {
         </div>
       </MyLayout>
     );
-  } else
+  } else {
     return (
-      <>
-        <MyLayout>
-          <div className="titlesearch">
-            <Heading heading="Places to stay" />
-            <div className="filterdiv">
-              <input
-                className="filter"
-                type="text"
-                name="query"
-                onChange={handleFiltering}
-                placeholder="Search..."
-              />
-            </div>
-          </div>
-          {hotels.map((hotel) => {
-            return (
-              <div key={hotel.id} className="hoteldiv">
-                <Link to={`/places-to-stay/${hotel.id}`} className="hotelinfo">
-                  <Row gutter={[16, 16]}>
-                    <Col sm={24} lg={9}>
-                      <div>
-                        <img
-                          src={hotel.img}
-                          alt={hotel.name}
-                          style={{ width: "100%" }}
-                          className="hotelimg"
-                        />
-                      </div>
-                    </Col>
-                    <Col sm={24} lg={11}>
-                      <p className="text">{hotel.shortdescription}</p>
-                      <h3 className="biggertext">{hotel.name}</h3>
-                      <hr className="line"></hr>
-                      <br></br>
-                      <p className="extras text">{hotel.extras}</p>
-                      <p className="text">
-                        {hotel.distance}km from Bergen city centre
-                      </p>
-                    </Col>
-                    <Col sm={24} lg={4}>
-                      <p className="price biggertext">{hotel.price} NOK</p>
-                      <p className="taxes text">Per night including taxes</p>
-                    </Col>
-                  </Row>
-                </Link>
-              </div>
-            );
-          })}
-        </MyLayout>
-      </>
+      <MyLayout>
+        <div>
+          <p>There has been an error. We apologize for the inconvenience.</p>
+        </div>
+      </MyLayout>
     );
+  }
 };
 
 export default Places;
