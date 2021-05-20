a051524da23da967f01fd02cf50093a7202c1b1b
Yvonne H <yvonnehelland@outlook.com>
2021-05-20 16:07:48 +0200

    Code cleanup/adds comments for readability

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -9,6 +9,7 @@ const Favorites = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  // fetch hotels
   useEffect(() => {
     const fetchHotels = async () => {
       try {
@@ -24,56 +25,60 @@ const Favorites = () => {
     fetchHotels();
   }, []);
 
+  // returns
   if (loading) {
     return (
       <div>
         <p>The content is loading... Please hold on.</p>
       </div>
     );
-  }
-
-  if (error) {
+  } else if (error) {
     return (
       <div>
         <p>There has been an error. We apologize for the inconvenience.</p>
       </div>
     );
-  }
-
-  return (
-    <>
-      <h3 style={{ textAlign: "center" }}>CUSTOMER FAVORITES</h3>
-      <Row gutter={[32, 32]}>
-        {hotels.map((hotel) => {
-          if (hotel.favorite) {
-            return (
-              <Col key={hotel.id} xs={24} sm={12} lg={6}>
-                <Link to={`/places-to-stay/${hotel.id}`} className="hotelinfo">
-                  <div
-                    className="favoritesimg"
-                    style={{
-                      width: "100%",
-                      paddingBottom: "100%",
-                      backgroundImage: `url('${hotel.img}')`,
-                      backgroundSize: "cover",
-                    }}
-                  ></div>
-                </Link>
-                <Link to={`/places-to-stay/${hotel.id}`} className="hotelinfo">
-                  <h3
-                    className="name h3link"
-                    style={{ textAlign: "center", padding: "10px" }}
+  } else
+    return (
+      <>
+        <h3 style={{ textAlign: "center" }}>CUSTOMER FAVORITES</h3>
+        <Row gutter={[32, 32]}>
+          {hotels.map((hotel) => {
+            if (hotel.favorite) {
+              return (
+                <Col key={hotel.id} xs={24} sm={12} lg={6}>
+                  <Link
+                    to={`/places-to-stay/${hotel.id}`}
+                    className="hotelinfo"
                   >
-                    {hotel.name}
-                  </h3>
-                </Link>
-              </Col>
-            );
-          } else return "";
-        })}
-      </Row>
-    </>
-  );
+                    <div
+                      className="favoritesimg"
+                      style={{
+                        width: "100%",
+                        paddingBottom: "100%",
+                        backgroundImage: `url('${hotel.img}')`,
+                        backgroundSize: "cover",
+                      }}
+                    ></div>
+                  </Link>
+                  <Link
+                    to={`/places-to-stay/${hotel.id}`}
+                    className="hotelinfo"
+                  >
+                    <h3
+                      className="name h3link"
+                      style={{ textAlign: "center", padding: "10px" }}
+                    >
+                      {hotel.name}
+                    </h3>
+                  </Link>
+                </Col>
+              );
+            } else return "";
+          })}
+        </Row>
+      </>
+    );
 };
 
 export default Favorites;
